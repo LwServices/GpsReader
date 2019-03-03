17171cf2859cd0a9e90bd83812cd3e4332e8ca4a
Lech Wiecierzynski <wiecierzynski@gmail.com>
2019-03-03 06:56:27 +0100

    Refactor and Organize

--- ConsoleGpsReader/Program.cs
+++ ConsoleGpsReader/Program.cs
@@ -1,87 +1,22 @@
-﻿using NmeaParser;
-using NmeaParser.Nmea;
-using NmeaParser.Nmea.Gps;
-using System;
-using System.Collections.Generic;
-using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace ConsoleGpsReader
 {
-    internal class Program
+    internal static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            var sw = new SerialWrapper();
-            sw.InitPort(9600);
-            sw.InitGps();
+            var serialWrapper = new SerialWrapper();
+            if (serialWrapper.InitPort())
+            {
+                serialWrapper.InitGps();
+            }
+            else
+            {
+                Console.WriteLine("No SerialPort's found");
+            }
 
             Console.ReadKey();
-        }
-    }
-
-    public class SerialWrapper
-    {
-        private const int BaudRate = 9600;
-        private SerialPort _serialPort;
-
-        public SerialWrapper()
-        {
-        }
-
-        public bool InitPort(int baudRate)
-        {
-            var portNames = SerialPort.GetPortNames();
-            if (portNames.Any())
-            {
-                var firstPort = portNames[0];
-                Console.WriteLine($"First port is {firstPort}");
-                return InitPort(firstPort, BaudRate);
-            }
-            Console.WriteLine("No Serial Ports Found");
-
-            return false;
-        }
-
-        public bool InitPort(string portName, int baudRate)
-        {
-            _serialPort = new SerialPort(portName, BaudRate);
-            return _serialPort.IsOpen;
-        }
-
-        public bool InitGps()
-        {
-            var device = new SerialPortDevice(_serialPort);
-            device.MessageReceived += DeviceOnMessageReceived;
-            device.OpenAsync();
-
-            return true;
-        }
-
-        private void DeviceOnMessageReceived(object sender, NmeaMessageReceivedEventArgs e)
-
-        {
-            if (e.Message is Gpgsa gpgsa)
-            {
-                Console.WriteLine(gpgsa.GpsMode);
-            }
-
-            if (e.Message is Gpgll gpgll)
-            {
-                Console.WriteLine(gpgll.FixTime);
-            }
-
-            if (e.Message is Gprmc gprmc)
-            {
-                Console.WriteLine(gprmc.Course);
-            }
-
-            if (e.Message is Gpgga gpgga)
-            {
-                Console.WriteLine(gpgga.Hdop);
-            }
         }
     }
 }
---
+++ ConsoleGpsReader/SerialWrapper.cs
@@ -0,0 +1,69 @@
+﻿using NmeaParser;
+using NmeaParser.Nmea.Gps;
+using System;
+using System.IO.Ports;
+
+namespace ConsoleGpsReader
+{
+    public class SerialWrapper
+    {
+        private const int BaudRate = 9600;
+        private SerialPort _serialPort;
+
+        public bool InitPort()
+        {
+            return InitPort(BaudRate);
+        }
+
+        public bool InitPort(int baudRate)
+        {
+            var portNames = SerialPort.GetPortNames();
+            if (portNames.Length > 0)
+            {
+                var firstPort = portNames[0];
+                Console.WriteLine($"First port is {firstPort}");
+                return InitPort(firstPort, baudRate);
+            }
+            return false;
+        }
+
+        public bool InitPort(string portName, int baudRate)
+        {
+            _serialPort = new SerialPort(portName, baudRate);
+            return _serialPort.IsOpen;
+        }
+
+        public bool InitGps()
+        {
+            var device = new SerialPortDevice(_serialPort);
+            device.MessageReceived += DeviceOnMessageReceived;
+            device.OpenAsync();
+
+            return true;
+        }
+
+        private void DeviceOnMessageReceived(object sender, NmeaMessageReceivedEventArgs e)
+
+        {
+            if (e.Message is Gpgsa gpgsa)
+            {
+                Console.WriteLine(gpgsa.GpsMode);
+            }
+
+            if (e.Message is Gpgll gpgll)
+            {
+                Console.WriteLine(gpgll.FixTime);
+            }
+
+            if (e.Message is Gprmc gprmc)
+            {
+                Console.WriteLine(gprmc.Course);
+            }
+
+            if (e.Message is Gpgga gpgga)
+            {
+                Console.WriteLine(gpgga.Hdop);
+            }
+        }
+    }
+}
